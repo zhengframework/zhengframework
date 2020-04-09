a5cc24a4a5a3a88cf3dfb62fa99258289d7d1c19
ysykzheng <zhengminghai@new4g.cn>
2020-04-09 10:13:57 +0800

    querydsl

---
+++ zheng-jdbc/src/main/java/com/dadazhishi/zheng/jdbc/QuerydslModule.java
@@ -0,0 +1,38 @@
+package com.dadazhishi.zheng.jdbc;
+
+import static com.google.inject.name.Names.named;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Key;
+import com.querydsl.sql.SQLQueryFactory;
+import java.lang.annotation.Annotation;
+import java.util.Objects;
+import lombok.EqualsAndHashCode;
+
+/**
+ * http://www.querydsl.com/static/querydsl/latest/reference/html/ch02s03.html
+ */
+@EqualsAndHashCode(callSuper = false, of = {"qualifier"})
+public class QuerydslModule extends AbstractModule {
+
+  private Annotation qualifier;
+
+  public QuerydslModule(Annotation qualifier) {
+    this.qualifier = qualifier;
+  }
+
+  public QuerydslModule(String name) {
+    this.qualifier = named(Objects.requireNonNull(name));
+  }
+
+
+  @Override
+  protected void configure() {
+    SQLQueryFactoryProvider sqlQueryFactoryProvider = new SQLQueryFactoryProvider(qualifier);
+    if (qualifier == null) {
+      bind(Key.get(SQLQueryFactory.class)).toProvider(sqlQueryFactoryProvider);
+    } else {
+      bind(Key.get(SQLQueryFactory.class, qualifier)).toProvider(sqlQueryFactoryProvider);
+    }
+  }
+}
---
+++ zheng-jdbc/src/main/java/com/dadazhishi/zheng/jdbc/SQLQueryFactoryProvider.java
@@ -0,0 +1,40 @@
+package com.dadazhishi.zheng.jdbc;
+
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.querydsl.sql.Configuration;
+import com.querydsl.sql.SQLQueryFactory;
+import com.querydsl.sql.SQLTemplates;
+import java.lang.annotation.Annotation;
+import javax.inject.Inject;
+import javax.inject.Provider;
+import javax.sql.DataSource;
+
+public class SQLQueryFactoryProvider implements Provider<SQLQueryFactory> {
+
+  private final Annotation qualifier;
+
+  @Inject
+  private Injector injector;
+
+  public SQLQueryFactoryProvider(Annotation qualifier) {
+    this.qualifier = qualifier;
+  }
+
+
+  @Override
+  public SQLQueryFactory get() {
+    DataSource dataSource;
+    SQLTemplates sqlTemplates;
+    if (qualifier == null) {
+      dataSource = injector.getInstance(Key.get(DataSource.class));
+      sqlTemplates = injector.getInstance(Key.get(SQLTemplates.class));
+    } else {
+      dataSource = injector
+          .getInstance(Key.get(DataSource.class, qualifier));
+      sqlTemplates = injector.getInstance(Key.get(SQLTemplates.class, qualifier));
+    }
+    Configuration configuration = new Configuration(sqlTemplates);
+    return new SQLQueryFactory(configuration, dataSource);
+  }
+}
