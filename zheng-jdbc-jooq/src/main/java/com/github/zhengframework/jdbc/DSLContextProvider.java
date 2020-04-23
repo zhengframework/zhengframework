c422d5d569e8df193504424e771696a0653ba54d
ysykzheng <zhengminghai@new4g.cn>
2020-04-23 22:53:14 +0800

    update JooqModule

--- zheng-jdbc-jooq/src/main/java/com/github/zhengframework/jdbc/DSLContextProvider.java
+++ zheng-jdbc-jooq/src/main/java/com/github/zhengframework/jdbc/DSLContextProvider.java
@@ -1,39 +1,29 @@
 package com.github.zhengframework.jdbc;
 
-import com.google.inject.Injector;
-import com.google.inject.Key;
-import java.lang.annotation.Annotation;
+import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.sql.DataSource;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.conf.Settings;
 import org.jooq.impl.DSL;
 
 public class DSLContextProvider implements Provider<DSLContext> {
 
-  private final Annotation qualifier;
+  private Provider<DataSource> dataSourceProvider;
+  private Provider<SQLDialect> sqlDialectProvider;
+  private Provider<Settings> settingsProvider;
 
-  private final Provider<Injector> injectorProvider;
-
-  public DSLContextProvider(Annotation qualifier,
-      Provider<Injector> injectorProvider) {
-    this.qualifier = qualifier;
-    this.injectorProvider = injectorProvider;
+  @Inject
+  public DSLContextProvider(Provider<DataSource> dataSourceProvider,
+      Provider<SQLDialect> sqlDialectProvider, Provider<Settings> settingsProvider) {
+    this.dataSourceProvider = dataSourceProvider;
+    this.sqlDialectProvider = sqlDialectProvider;
+    this.settingsProvider = settingsProvider;
   }
 
   @Override
   public DSLContext get() {
-    DataSource dataSource;
-    SQLDialect sqlDialect;
-    Injector injector = injectorProvider.get();
-    if (qualifier == null) {
-      dataSource = injector.getInstance(Key.get(DataSource.class));
-      sqlDialect = injector.getInstance(Key.get(SQLDialect.class));
-    } else {
-      dataSource = injector
-          .getInstance(Key.get(DataSource.class, qualifier));
-      sqlDialect = injector.getInstance(Key.get(SQLDialect.class, qualifier));
-    }
-    return DSL.using(dataSource, sqlDialect);
+    return DSL.using(dataSourceProvider.get(), sqlDialectProvider.get(), settingsProvider.get());
   }
 }
--- zheng-jdbc-jooq/src/main/java/com/github/zhengframework/jdbc/JooqModule.java
+++ zheng-jdbc-jooq/src/main/java/com/github/zhengframework/jdbc/JooqModule.java
@@ -2,46 +2,60 @@ package com.github.zhengframework.jdbc;
 
 import static com.google.inject.name.Names.named;
 
+import com.github.zhengframework.configuration.Configuration;
+import com.github.zhengframework.configuration.ConfigurationAware;
+import com.github.zhengframework.configuration.ConfigurationBeanMapper;
+import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
-import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.multibindings.OptionalBinder;
-import java.lang.annotation.Annotation;
-import java.util.Objects;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.sql.DataSource;
 import lombok.EqualsAndHashCode;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
+import org.jooq.conf.Settings;
 
 @EqualsAndHashCode(callSuper = false)
-public class JooqModule extends AbstractModule {
+public class JooqModule extends AbstractModule implements ConfigurationAware {
 
-  private Annotation qualifier;
-
-  public JooqModule() {
-    this.qualifier = null;
-  }
-
-  public JooqModule(Annotation qualifier) {
-    this.qualifier = qualifier;
-  }
-
-  public JooqModule(String name) {
-    this.qualifier = named(Objects.requireNonNull(name));
-  }
+  private Configuration configuration;
 
   @Override
   protected void configure() {
-    DSLContextProvider dslContextProvider = new DSLContextProvider(qualifier,
-        getProvider(Injector.class));
-    if (qualifier == null) {
-      OptionalBinder.newOptionalBinder(binder(), Key.get(SQLDialect.class))
-          .setDefault().toInstance(SQLDialect.DEFAULT);
-      bind(Key.get(DSLContext.class)).toProvider(dslContextProvider);
-    } else {
-      OptionalBinder.newOptionalBinder(binder(), Key.get(SQLDialect.class, qualifier))
-          .setDefault().toInstance(SQLDialect.DEFAULT);
-      bind(Key.get(DSLContext.class, qualifier)).toProvider(dslContextProvider);
+    Preconditions.checkArgument(configuration != null, "configuration is null");
+    Map<String, JooqConfig> configMap = ConfigurationBeanMapper
+        .resolve(configuration, JooqConfig.class);
+    for (Entry<String, JooqConfig> entry : configMap.entrySet()) {
+      String name = entry.getKey();
+      JooqConfig jooqConfig = entry.getValue();
+      if (name.isEmpty()) {
+        bind(JooqConfig.class).toInstance(jooqConfig);
+        if (jooqConfig.isEnable()) {
+          OptionalBinder.newOptionalBinder(binder(), Key.get(SQLDialect.class))
+              .setDefault().toInstance(SQLDialect.DEFAULT);
+          bind(Key.get(DSLContext.class))
+              .toProvider(new DSLContextProvider(getProvider(Key.get(DataSource.class))
+                  , getProvider(Key.get(SQLDialect.class))
+                  , getProvider(Key.get(Settings.class))));
+        }
+      } else {
+        bind(Key.get(JooqConfig.class, named(name))).toInstance(jooqConfig);
+        if (jooqConfig.isEnable()) {
+          OptionalBinder.newOptionalBinder(binder(), Key.get(SQLDialect.class, named(name)))
+              .setDefault().toInstance(SQLDialect.DEFAULT);
+          bind(Key.get(DSLContext.class, named(name)))
+              .toProvider(new DSLContextProvider(getProvider(Key.get(DataSource.class, named(name)))
+                  , getProvider(Key.get(SQLDialect.class, named(name)))
+                  , getProvider(Key.get(Settings.class))));
+        }
+      }
     }
+  }
 
+  @Override
+  public void initConfiguration(Configuration configuration) {
+    this.configuration = configuration;
   }
 }
