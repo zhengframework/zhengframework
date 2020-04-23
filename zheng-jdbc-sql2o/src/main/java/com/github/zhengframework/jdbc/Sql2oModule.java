3e89dca3f9c59c55274569707e6b798a45546260
ysykzheng <zhengminghai@new4g.cn>
2020-04-23 22:31:18 +0800

    update Sql2oModule logic

--- zheng-jdbc-sql2o/src/main/java/com/github/zhengframework/jdbc/Sql2oModule.java
+++ zheng-jdbc-sql2o/src/main/java/com/github/zhengframework/jdbc/Sql2oModule.java
@@ -10,6 +10,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Key;
 import java.util.Map;
 import java.util.Map.Entry;
+import javax.inject.Singleton;
 import javax.sql.DataSource;
 import lombok.EqualsAndHashCode;
 import org.sql2o.Sql2o;
@@ -34,12 +35,16 @@ public class Sql2oModule extends AbstractModule implements ConfigurationAware {
       Sql2oConfig sql2oConfig = entry.getValue();
       if (name.isEmpty()) {
         bind(Sql2oConfig.class).toInstance(sql2oConfig);
-        bind(Sql2o.class).toProvider(new Sql2oProvider(
-            getProvider(DataSource.class)));
+        if (sql2oConfig.isEnable()) {
+          bind(Sql2o.class).toProvider(new Sql2oProvider(
+              getProvider(DataSource.class))).in(Singleton.class);
+        }
       } else {
         bind(Key.get(Sql2oConfig.class, named(name))).toInstance(sql2oConfig);
-        bind(Key.get(Sql2o.class, named(name))).toProvider(new Sql2oProvider(
-            getProvider(Key.get(DataSource.class, named(name)))));
+        if (sql2oConfig.isEnable()) {
+          bind(Key.get(Sql2o.class, named(name))).toProvider(new Sql2oProvider(
+              getProvider(Key.get(DataSource.class, named(name))))).in(Singleton.class);
+        }
       }
     }
   }
--- zheng-jdbc-sql2o/src/main/java/com/github/zhengframework/jdbc/Sql2oProvider.java
+++ zheng-jdbc-sql2o/src/main/java/com/github/zhengframework/jdbc/Sql2oProvider.java
@@ -1,5 +1,6 @@
 package com.github.zhengframework.jdbc;
 
+import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.sql.DataSource;
 import org.sql2o.Sql2o;
@@ -8,6 +9,7 @@ public class Sql2oProvider implements Provider<Sql2o> {
 
   private Provider<DataSource> dataSourceProvider;
 
+  @Inject
   public Sql2oProvider(Provider<DataSource> dataSourceProvider) {
     this.dataSourceProvider = dataSourceProvider;
   }
