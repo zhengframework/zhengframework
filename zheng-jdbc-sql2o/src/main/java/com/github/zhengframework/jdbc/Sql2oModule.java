2a7c8cf374903e57dd7f352ffcc15751f3bb8dbf
ysykzheng <zhengminghai@new4g.cn>
2020-04-23 22:16:36 +0800

    update Sql2oModule

--- zheng-jdbc-sql2o/src/main/java/com/github/zhengframework/jdbc/Sql2oModule.java
+++ zheng-jdbc-sql2o/src/main/java/com/github/zhengframework/jdbc/Sql2oModule.java
@@ -2,11 +2,15 @@ package com.github.zhengframework.jdbc;
 
 import static com.google.inject.name.Names.named;
 
+import com.github.zhengframework.configuration.Configuration;
+import com.github.zhengframework.configuration.ConfigurationAware;
+import com.github.zhengframework.configuration.ConfigurationBeanMapper;
+import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
-import com.google.inject.Injector;
 import com.google.inject.Key;
-import java.lang.annotation.Annotation;
-import java.util.Objects;
+import java.util.Map;
+import java.util.Map.Entry;
+import javax.sql.DataSource;
 import lombok.EqualsAndHashCode;
 import org.sql2o.Sql2o;
 
@@ -14,30 +18,34 @@ import org.sql2o.Sql2o;
  * https://github.com/aaberg/sql2o
  */
 @EqualsAndHashCode(callSuper = false)
-public class Sql2oModule extends AbstractModule {
+public class Sql2oModule extends AbstractModule implements ConfigurationAware {
 
-  private Annotation qualifier;
-
-  public Sql2oModule(Annotation qualifier) {
-    this.qualifier = qualifier;
-  }
-
-  public Sql2oModule() {
-    this.qualifier = null;
-  }
-
-  public Sql2oModule(String name) {
-    this.qualifier = named(Objects.requireNonNull(name));
-  }
+  private Configuration configuration;
 
   @Override
   protected void configure() {
-    Sql2oProvider sql2oProvider = new Sql2oProvider(qualifier,
-        getProvider(Injector.class));
-    if (qualifier == null) {
-      bind(Key.get(Sql2o.class)).toProvider(sql2oProvider);
-    } else {
-      bind(Key.get(Sql2o.class, qualifier)).toProvider(sql2oProvider);
+
+    Preconditions.checkArgument(configuration != null, "configuration is null");
+
+    Map<String, Sql2oConfig> sql2oConfigMap = ConfigurationBeanMapper
+        .resolve(configuration, Sql2oConfig.class);
+    for (Entry<String, Sql2oConfig> entry : sql2oConfigMap.entrySet()) {
+      String name = entry.getKey();
+      Sql2oConfig sql2oConfig = entry.getValue();
+      if (name.isEmpty()) {
+        bind(Sql2oConfig.class).toInstance(sql2oConfig);
+        bind(Sql2o.class).toProvider(new Sql2oProvider(
+            getProvider(DataSource.class)));
+      } else {
+        bind(Key.get(Sql2oConfig.class, named(name))).toInstance(sql2oConfig);
+        bind(Key.get(Sql2o.class, named(name))).toProvider(new Sql2oProvider(
+            getProvider(Key.get(DataSource.class, named(name)))));
+      }
     }
+  }
+
+  @Override
+  public void initConfiguration(Configuration configuration) {
+    this.configuration = configuration;
   }
 }
---
+++ zheng-jdbc-sql2o/src/test/java/com/github/zhengframework/jdbc/Sql2oModuleTest.java
@@ -0,0 +1,69 @@
+package com.github.zhengframework.jdbc;
+
+import static com.google.inject.name.Names.named;
+import static org.junit.Assert.assertNotNull;
+
+import com.github.zhengframework.bootstrap.ZhengApplication;
+import com.github.zhengframework.bootstrap.ZhengApplicationBuilder;
+import com.github.zhengframework.configuration.Configuration;
+import com.github.zhengframework.configuration.ConfigurationBuilder;
+import com.github.zhengframework.configuration.source.FileConfigurationSource;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.sql2o.Sql2o;
+
+public class Sql2oModuleTest {
+
+  @Test
+  public void configure() throws SQLException {
+    Configuration configuration = new ConfigurationBuilder()
+        .withConfigurationSource(new FileConfigurationSource("application.properties"))
+        .build();
+    System.out.println(configuration.asMap());
+
+    ZhengApplication application = ZhengApplicationBuilder.create()
+        .enableAutoLoadModule()
+        .withConfiguration(configuration)
+        .build();
+    DataSource dataSource = application.getInjector().getInstance(DataSource.class);
+    Connection connection = dataSource.getConnection();
+    DatabaseMetaData metaData = connection.getMetaData();
+    System.out.println(metaData.getDatabaseProductName());
+    Assert.assertEquals("HSQL Database Engine", metaData.getDatabaseProductName());
+    Sql2o instance = application.getInjector().getInstance(Sql2o.class);
+    assertNotNull(instance);
+  }
+
+  @Test
+  public void configureGroup() throws SQLException {
+    Configuration configuration = new ConfigurationBuilder()
+        .withConfigurationSource(new FileConfigurationSource("application_group.properties"))
+        .build();
+    ZhengApplication application = ZhengApplicationBuilder.create()
+        .addModule(new DataSourceModule())
+        .enableAutoLoadModule()
+        .withConfiguration(configuration)
+        .build();
+    Injector injector = application.getInjector();
+    DataSource dataSourceA = injector
+        .getInstance(Key.get(DataSource.class, named("a")));
+    DatabaseMetaData metaDataA = dataSourceA.getConnection().getMetaData();
+    System.out.println(metaDataA.getDatabaseProductName());
+    Assert.assertEquals("HSQL Database Engine", metaDataA.getDatabaseProductName());
+
+    DataSource dataSourceB = injector
+        .getInstance(Key.get(DataSource.class, named("b")));
+    DatabaseMetaData metaDataB = dataSourceB.getConnection().getMetaData();
+    Assert.assertEquals(metaDataB.getDatabaseProductName(), metaDataA.getDatabaseProductName());
+    Assert.assertNotEquals(dataSourceA, dataSourceB);
+    assertNotNull(injector.getInstance(Key.get(Sql2o.class, named("a"))));
+    assertNotNull(injector.getInstance(Key.get(Sql2o.class, named("b"))));
+
+  }
+}
