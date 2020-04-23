28b90eab17515a2394225bd117400c976df24b84
ysykzheng <zhengminghai@new4g.cn>
2020-04-23 17:35:06 +0800

    update FlywayMigrateModule

--- zheng-migrate-flyway/src/main/java/com/github/zhengframework/jdbc/FlywayManagedSchema.java
+++ zheng-migrate-flyway/src/main/java/com/github/zhengframework/jdbc/FlywayManagedSchema.java
@@ -1,5 +1,6 @@
 package com.github.zhengframework.jdbc;
 
+import javax.inject.Inject;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
@@ -8,23 +9,32 @@ import org.flywaydb.core.api.MigrationInfo;
 @Slf4j
 public class FlywayManagedSchema implements ManagedSchema {
 
+  private final FlywayConfig flywayConfig;
+
+  @Inject
+  public FlywayManagedSchema(FlywayConfig flywayConfig) {
+    this.flywayConfig = flywayConfig;
+  }
+
   @Override
   public void migrate(DataSource dataSource) {
-    log.info("Starting DB migration");
+    log.info("flywayConfig={}", flywayConfig);
+    if (flywayConfig.isEnable()) {
+      log.info("Starting DB migration");
+      Flyway flyway = Flyway.configure().dataSource(dataSource)
+          .locations(flywayConfig.getLocations())
+          .load();
 
-    Flyway flyway = Flyway.configure().dataSource(dataSource)
-        .loadDefaultConfigurationFiles()
-        .load();
+      MigrationInfo current = flyway.info().current();
+      if (current == null) {
+        log.info("No existing schema found");
+      } else {
+        log.info("Current schema version is {}", current.getVersion());
+      }
 
-    MigrationInfo current = flyway.info().current();
-    if (current == null) {
-      log.info("No existing schema found");
-    } else {
-      log.info("Current schema version is {}", current.getVersion());
+      flyway.migrate();
+
+      log.info("Schema migrated to version {}", flyway.info().current().getVersion());
     }
-
-    flyway.migrate();
-
-    log.info("Schema migrated to version {}", flyway.info().current().getVersion());
   }
 }
--- zheng-migrate-flyway/src/main/java/com/github/zhengframework/jdbc/FlywayMigrateModule.java
+++ zheng-migrate-flyway/src/main/java/com/github/zhengframework/jdbc/FlywayMigrateModule.java
@@ -1,13 +1,49 @@
 package com.github.zhengframework.jdbc;
 
+import static com.google.inject.name.Names.named;
+
+import com.github.zhengframework.configuration.Configuration;
+import com.github.zhengframework.configuration.ConfigurationAware;
+import com.github.zhengframework.configuration.ConfigurationBeanMapper;
+import com.google.common.base.Preconditions;
 import com.google.inject.AbstractModule;
+import com.google.inject.Key;
+import com.google.inject.multibindings.OptionalBinder;
+import java.util.Map;
+import java.util.Map.Entry;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = false)
-public class FlywayMigrateModule extends AbstractModule {
+public class FlywayMigrateModule extends AbstractModule implements ConfigurationAware {
+
+  private Configuration configuration;
 
   @Override
   protected void configure() {
-    bind(ManagedSchema.class).to(FlywayManagedSchema.class);
+    Preconditions.checkArgument(configuration != null, "configuration is null");
+
+    Map<String, FlywayConfig> metricsConfigMap = ConfigurationBeanMapper
+        .resolve(configuration, FlywayConfig.class);
+    for (Entry<String, FlywayConfig> entry : metricsConfigMap.entrySet()) {
+      String name = entry.getKey();
+      FlywayConfig flywayConfig = entry.getValue();
+      if (name.isEmpty()) {
+        bind(FlywayConfig.class).toInstance(flywayConfig);
+        OptionalBinder.newOptionalBinder(binder(), Key.get(ManagedSchema.class))
+            .setBinding().toInstance(new FlywayManagedSchema(flywayConfig));
+      } else {
+        bind(Key.get(FlywayConfig.class, named(name))).toInstance(flywayConfig);
+        OptionalBinder.newOptionalBinder(binder(), Key.get(ManagedSchema.class, named(name)))
+            .setBinding().toInstance(new FlywayManagedSchema(flywayConfig));
+      }
+
+    }
+
+
+  }
+
+  @Override
+  public void initConfiguration(Configuration configuration) {
+    this.configuration = configuration;
   }
 }
