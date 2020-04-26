16893e5fc80165b1f62e2be4312750fdb9901d47
ysykzheng <zhengminghai@new4g.cn>
2020-04-26 21:33:23 +0800

    change unit test by ZhengApplicationRunner

--- zheng-migrate-flyway/src/test/java/com/github/zhengframework/jdbc/migrate/FlywayMigrateModuleTest.java
+++ zheng-migrate-flyway/src/test/java/com/github/zhengframework/jdbc/migrate/FlywayMigrateModuleTest.java
@@ -1,27 +1,28 @@
 package com.github.zhengframework.jdbc.migrate;
 
-import com.github.zhengframework.bootstrap.ZhengApplication;
-import com.github.zhengframework.bootstrap.ZhengApplicationBuilder;
+import com.github.zhengframework.test.WithZhengApplication;
+import com.github.zhengframework.test.ZhengApplicationRunner;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import javax.sql.DataSource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(ZhengApplicationRunner.class)
 public class FlywayMigrateModuleTest {
 
-  @org.junit.Test
+  @Inject
+  private Injector injector;
+
+  @WithZhengApplication
+  @Test
   public void configure() throws Exception {
-    ZhengApplication application = ZhengApplicationBuilder.create().addModule(
-        new FlywayMigrateModule())
-        .enableAutoLoadModule()
-        .build();
-    application.start();
-    Injector injector = application.getInjector();
     DataSource dataSource = injector.getInstance(DataSource.class);
     Connection connection = dataSource.getConnection();
     DatabaseMetaData metaData = connection.getMetaData();
     System.out.println(metaData.getDatabaseProductName());
     connection.close();
-    application.stop();
   }
 }
--- zheng-migrate-liquibase/src/test/java/com/github/zhengframework/jdbc/migrate/LiquibaseMigrateModuleTest.java
+++ zheng-migrate-liquibase/src/test/java/com/github/zhengframework/jdbc/migrate/LiquibaseMigrateModuleTest.java
@@ -2,8 +2,9 @@ package com.github.zhengframework.jdbc.migrate;
 
 import static org.junit.Assert.assertTrue;
 
-import com.github.zhengframework.bootstrap.ZhengApplication;
-import com.github.zhengframework.bootstrap.ZhengApplicationBuilder;
+import com.github.zhengframework.test.WithZhengApplication;
+import com.github.zhengframework.test.ZhengApplicationRunner;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -11,17 +12,17 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.sql.DataSource;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(ZhengApplicationRunner.class)
 public class LiquibaseMigrateModuleTest {
 
+  @Inject
+  private Injector injector;
+
+  @WithZhengApplication
   @Test
   public void configure() throws Exception {
-    ZhengApplication application = ZhengApplicationBuilder.create().addModule(
-        new LiquibaseMigrateModule())
-        .enableAutoLoadModule()
-        .build();
-    application.start();
-    Injector injector = application.getInjector();
     DataSource dataSource = injector.getInstance(DataSource.class);
     Connection connection = dataSource.getConnection();
     DatabaseMetaData metaData = connection.getMetaData();
@@ -32,6 +33,5 @@ public class LiquibaseMigrateModuleTest {
     resultSet.close();
     statement.close();
     connection.close();
-    application.stop();
   }
 }
