cf9aed95956a052d60a6aac920810abfb23bd5d9
ysykzheng <zhengminghai@new4g.cn>
2020-04-30 22:11:27 +0800

    change ConfigurationBeanMapper

--- zheng-configuration/src/test/java/com/github/zhengframework/configuration/ConfigurationDefineExample.java
+++ zheng-configuration/src/test/java/com/github/zhengframework/configuration/ConfigurationDefineExample.java
@@ -1,7 +1,13 @@
 package com.github.zhengframework.configuration;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.zhengframework.configuration.ConfigurationDefineExample.DefaultConfigurationDefineExample;
+import com.github.zhengframework.configuration.ConfigurationDefineExample.FirstConfigurationDefineExample;
+import com.github.zhengframework.configuration.ConfigurationDefineExample.SecondConfigurationDefineExample;
+import com.github.zhengframework.configuration.annotation.ConfigurationExample;
 import com.github.zhengframework.configuration.annotation.ConfigurationInfo;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,21 +15,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @ConfigurationInfo(prefix = "zheng.example", supportGroup = true, examples = {
-//    @ConfigurationExample(example = ConfigurationDefineExample.class),
-//    @ConfigurationExample(groupName = "first", example = ConfigurationDefineExample.class),
-//    @ConfigurationExample(groupName = "second", example = ConfigurationDefineExample.class)
+    @ConfigurationExample(example = DefaultConfigurationDefineExample.class),
+    @ConfigurationExample(groupName = "first", example = FirstConfigurationDefineExample.class),
+    @ConfigurationExample(groupName = "second", example = SecondConfigurationDefineExample.class)
 })
 @Data
 @NoArgsConstructor
 public class ConfigurationDefineExample implements ConfigurationDefine {
 
-  public static final String PREFIX = "zheng.example";
-
   private String basePath = "/base";
 
   private boolean enableWeb = true;
 
-  private Map<String, String> properties = new HashMap<>();
+  @JsonIgnore
+  private Map<String, Integer> properties = new HashMap<>();
 
   private JettyConfig jettyConfig = new JettyConfig();
 
@@ -35,5 +40,38 @@ public class ConfigurationDefineExample implements ConfigurationDefine {
 
     private int port = 8080;
     private String serverName = "abc";
+  }
+
+  static class DefaultConfigurationDefineExample extends ConfigurationDefineExample {
+
+    public DefaultConfigurationDefineExample() {
+      Map<String, Integer> properties = new HashMap<>();
+      properties.put("a.1", 1);
+      properties.put("b.2", 2);
+      setProperties(properties);
+      getIntegerList().addAll(Arrays.asList(1, 2, 3));
+    }
+  }
+
+  static class FirstConfigurationDefineExample extends ConfigurationDefineExample {
+
+    public FirstConfigurationDefineExample() {
+      Map<String, Integer> properties = new HashMap<>();
+      properties.put("aFirst", 1);
+      properties.put("bFirst", 2);
+      setProperties(properties);
+      getIntegerList().addAll(Arrays.asList(0, 1, 2, 3));
+    }
+  }
+
+  static class SecondConfigurationDefineExample extends ConfigurationDefineExample {
+
+    public SecondConfigurationDefineExample() {
+      Map<String, Integer> properties = new HashMap<>();
+      properties.put("aSecond", 1);
+      properties.put("bSecond", 2);
+      setProperties(properties);
+      getIntegerList().addAll(Arrays.asList(1, 2, 3, 4));
+    }
   }
 }
