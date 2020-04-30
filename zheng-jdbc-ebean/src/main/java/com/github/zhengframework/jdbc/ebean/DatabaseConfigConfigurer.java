bb16c1f7f8c97db55185989b0f6173d11e3207e2
ysykzheng <zhengminghai@new4g.cn>
2020-04-30 18:22:15 +0800

    add EbeanModule

---
+++ zheng-jdbc-ebean/src/main/java/com/github/zhengframework/jdbc/ebean/DatabaseConfigConfigurer.java
@@ -0,0 +1,8 @@
+package com.github.zhengframework.jdbc.ebean;
+
+import com.github.zhengframework.core.Configurer;
+import io.ebean.config.DatabaseConfig;
+
+public interface DatabaseConfigConfigurer extends Configurer<DatabaseConfig> {
+
+}
