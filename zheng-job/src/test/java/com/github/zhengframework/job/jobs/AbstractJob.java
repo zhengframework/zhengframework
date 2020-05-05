0cb86d26a6812332cf03800ea17bd9dca604e23b
ysykzheng <ysykart@gmail.com>
2020-05-05 16:15:04 +0800

    add license-maven-plugin

--- zheng-job/src/test/java/com/github/zhengframework/job/jobs/AbstractJob.java
+++ zheng-job/src/test/java/com/github/zhengframework/job/jobs/AbstractJob.java
@@ -1,5 +1,25 @@
 package com.github.zhengframework.job.jobs;
 
+/*-
+ * #%L
+ * zheng-job
+ * %%
+ * Copyright (C) 2020 Zheng MingHai
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.github.zhengframework.job.Job;
 import java.util.concurrent.CountDownLatch;
 import org.quartz.JobExecutionContext;
@@ -7,7 +27,7 @@ import org.quartz.JobExecutionException;
 
 public abstract class AbstractJob extends Job {
 
-    private final CountDownLatch latch;
+  private final CountDownLatch latch;
 
     public AbstractJob(int count) {
         super();
