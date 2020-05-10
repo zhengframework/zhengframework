b7b9d38de7772f0e35554538c61c7029b7866f3b
ysykzheng <ysykart@gmail.com>
2020-05-10 18:14:27 +0800

    JpaModule cleanup code

--- zheng-jpa/src/main/java/com/github/zhengframework/jpa/JpaModule.java
+++ zheng-jpa/src/main/java/com/github/zhengframework/jpa/JpaModule.java
@@ -24,7 +24,6 @@ import com.github.zhengframework.configuration.ConfigurationAwareModule;
 import com.github.zhengframework.configuration.ConfigurationBeanMapper;
 import com.github.zhengframework.guice.ExposedPrivateModule;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -32,9 +31,8 @@ public class JpaModule extends ConfigurationAwareModule {
 
   @Override
   protected void configure() {
-    Map<String, JpaConfig> persistenceConfigMap =
-        ConfigurationBeanMapper.resolve(getConfiguration(), JpaConfig.class);
-    JpaConfig jpaConfig = persistenceConfigMap.getOrDefault("", new JpaConfig());
+    JpaConfig jpaConfig =
+        ConfigurationBeanMapper.resolve(getConfiguration(), "", JpaConfig.class);
     if (jpaConfig.getPersistenceUnitName() == null) {
       jpaConfig.setPersistenceUnitName("zheng-jpa");
     }
@@ -44,7 +42,7 @@ public class JpaModule extends ConfigurationAwareModule {
     if (extraModuleClass != null) {
       try {
         ExposedPrivateModule module = extraModuleClass.getDeclaredConstructor().newInstance();
-        log.info("install extra module: " + extraModuleClass.getName());
+        log.info("install an extra module: " + extraModuleClass.getName());
         install(module);
       } catch (InstantiationException
           | IllegalAccessException
